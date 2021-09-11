2d5071cbe8e378406f5f76cca70b1a5a0e6ada8f
Ghulam <inferno2960@gmail.com>
2021-09-11 17:40:12 -0400

    updated animedoro to have a clock and use the redux store

--- src/animedoro/Animedoro.js
+++ src/animedoro/Animedoro.js
@@ -1,12 +1,46 @@
-
-
+import "./Animedoro.css";
+import React, { useState, useEffect } from "react";
+import AnimedoroCore from "./AnimedoroCore";
+import Navbar from "../navbar/Navbar";
+import taskService from "../services/tasks";
+import LoginForm from "../forms/loginForm";
 
 function Animedoro() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const loggedUserJson = window.localStorage.getItem("loggedTaskAppUser");
+    if (loggedUserJson) {
+      const user = JSON.parse(loggedUserJson);
+      setUser(user);
+      taskService.setToken(user.token);
+    }
+  }, []);
+
+  const loginUser = (user) => {
+    setUser(user);
+  };
+
+  const loginForm = () => {
     return (
       <div>
-          Animedoro
+        <LoginForm loginUser={loginUser} />
       </div>
     );
-  }
-  
-  export default Animedoro;
+  };
+
+  const logout = () => {
+    taskService.setToken(null);
+    window.localStorage.removeItem("loggedTaskAppUser");
+    setUser(null);
+
+  };
+  return (
+    <div>
+      <Navbar />
+      {user === null ? loginForm() : <AnimedoroCore logout={logout}/>}
+    </div>
+  );
+}
+
+export default Animedoro;
