0eb37eecce18f704f0769e95c8b1583db5b8c455
Ghulam <inferno2960@gmail.com>
2021-09-11 17:33:52 -0400

    updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
 import './App.css';
-// import { BrowserRouter as Router, Switch,  Route, Link } from "react-router-dom";
-// import Countdown from './countdown/Countdown.js'
-// import Animedoro from './animedoro/Animedoro.js'
-// import Stopwatch from './stopwatch/Stopwatch'
-import Home from './Home.js'
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Countdown from './countdown/Countdown.js';
+import Animedoro from './animedoro/Animedoro.js';
+import Stopwatch from './stopwatch/Stopwatch.js';
+import { SignUpPage } from './signUpPage/SignUpPage.js';
+import Home from './Home.js';
 
 function App() {
   return (
-    <Home />
-    // <Router>
-    //       <div>
-    //         <nav>
-    //           <ul>
-    //             <li>
-    //               <Link to="/">Home</Link>
-    //             </li>
-    //             <li>
-    //               <Link to="/Countdown">Countdown</Link>
-    //             </li>
-    //             <li>
-    //               <Link to="/Aniomedoro">Animedoro</Link>
-    //             </li>
-    //             <li>
-    //               <Link to="/stopwatch">Stopwatch</Link>
-    //             </li>
-    //           </ul>
-    //         </nav>
-    //         <Home />
-    //         {/* A <Switch> looks through its children <Route>s and
-    //             renders the first one that matches the current URL. */}
-    //         <Switch>
-    //         <Route path="/home">
-    //             <Home />
-    //           </Route>
-    //           <Route path="/Countdown">
-    //             <Countdown />
-    //           </Route>
-    //           <Route path="/Aniomedoro">
-    //             <Animedoro />
-    //           </Route>
-    //           <Route path="/stopwatch">
-    //             <Stopwatch />
-    //           </Route>
-    //         </Switch>
-    //       </div>
-    //     </Router>
-    // <div className="App">
-    //   <Countdown />
-    //   <Stopwatch /> 
-    //   <Animedoro />
-    // </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/countdown' component={Countdown} />
+          <Route path='/animedoro' component={Animedoro} />
+          <Route path='/stopwatch' component={Stopwatch} />
+          <Route path='/signup' component={SignUpPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
