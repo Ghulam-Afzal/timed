8e7be097538c65723753c3a70b1a21a84d16bbb8
Ghulam <inferno2960@gmail.com>
2021-09-16 15:59:33 -0400

    updated navbar to use styled components

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,46 +1,33 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
-import "./Navbar.css";
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from "./NavStyles";
+
 
 export default function Navbar(props) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="nav-container">
-      <nav>
-        <div className="logo">
-          <Link to="/" className="logo-link">
-            <div>TIMED</div>
-          </Link>
-        </div>
-        <ul
-          className="nav-links"
-          style={{ transform: open ? "translateX(0px)" : "translateX(-500px)" }}
-        >
-          <li>
-            <Link to="/countdown" className="link">
-              <span>Countdown</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/animedoro" className="link">
-              <span>Animedoro</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/stopwatch" className="link">
-              <span>Stopwatch</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/signup" >
-              <span className="nav-button">Sign Up</span>
-            </Link>
-          </li>
-        </ul>
-        <FaBars onClick={() => setOpen(!open)} className="burger" />
-      </nav>
+    <div>
+      <Nav>
+        <NavLink to="/" activeStyle>
+          TIMED
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/countdown" activeStyle>
+            Countdown
+          </NavLink>
+          <NavLink to="/animedoro" activeStyle>
+            Animedoro
+          </NavLink>
+          <NavLink to="/stopwatch" activeStyle>
+            Stopwatch
+          </NavLink>
+          <NavLink to="/signup" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+      </Nav>
     </div>
+    
   );
 }
